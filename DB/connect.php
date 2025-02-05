721b080cf9f4c25712071c0f6cee2eb1550a685c
Fernanda-echev <echeverriafernanda12@gmail.com>
2025-02-05 15:10:05 -0400

    slaaa

--- DB/connect.php
+++ DB/connect.php
@@ -1,6 +1,11 @@
 <?php
 
+<<<<<<< Updated upstream
 $conn= new mysqli('10.38.0.95', 'Echeverria', '321', 'crud_floric');
+=======
+$conn= new mysqli('10.38.0.95','Echeverria','321','crud_flor');
+// $conn= new mysqli('localhost', 'root', '', 'crud_flor');
+>>>>>>> Stashed changes
 
 
 if(!$conn){
