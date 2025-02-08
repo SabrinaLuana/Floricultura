02298d756572b8820f398d9b9e12bed71bb32c47
Fernanda-echev <echeverriafernanda12@gmail.com>
2025-02-07 23:24:14 -0300

    fer10

--- DB/connect.php
+++ DB/connect.php
@@ -1,7 +1,9 @@
 <?php
 
-// $conn= new mysqli('10.38.0.83', 'luana', '321', 'crud_floric');
-$conn= new mysqli('10.38.0.95', 'Echeverria', '321', 'crud_floric');
+// $conn= new mysqli('10.38.0.83', 'luana', '321', 'crud_flor');
+// $conn= new mysqli('10.38.0.95', 'Echeverria', '321', 'crud_flor');
+$conn= new mysqli('localhost', 'root', '', 'crud_floric');
+
 
 
 if(!$conn){
